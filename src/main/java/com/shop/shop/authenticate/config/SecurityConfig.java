32c271af3b2cd93562a4761367542c527095818c
tapash22 <tapasp263@gmail..com>
2026-04-25 16:18:05 +0800

    add @CrossOrigin at controller level
    Signed-off-by: tapash22 <tapasp263@gmail..com>

--- src/main/java/com/shop/shop/authenticate/config/SecurityConfig.java
+++ src/main/java/com/shop/shop/authenticate/config/SecurityConfig.java
@@ -1,73 +1,74 @@
-package com.shop.shop.authenticate.config;
+    package com.shop.shop.authenticate.config;
 
 
-import com.shop.shop.authenticate.filter.AuthFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+    import com.shop.shop.authenticate.filter.AuthFilter;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.data.domain.PageRequest;
+    import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+    import org.springframework.security.config.http.SessionCreationPolicy;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+    import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+    import org.springframework.web.servlet.config.annotation.CorsRegistry;
+    import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import java.util.List;
+    import java.util.List;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig extends WebMvcConfigurationSupport {
+    @Configuration
+    @EnableWebSecurity
+    @RequiredArgsConstructor
+    public class SecurityConfig extends WebMvcConfigurationSupport {
 
-    private final AuthFilter authFilter;
+        private final AuthFilter authFilter;
 
 
-    @Bean
-    public PasswordEncoder passwordEncoderBean () {
-        return new BCryptPasswordEncoder();
+        @Bean
+        public PasswordEncoder passwordEncoderBean () {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager (
+                AuthenticationConfiguration authConfig ) throws Exception {
+            return authConfig.getAuthenticationManager();
+        }
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(authorizeRequests ->
+                            authorizeRequests.requestMatchers("/authenticate").permitAll()
+                                    .requestMatchers("/users/**").permitAll()
+                                    .requestMatchers("/v2/**").permitAll()
+                                    .requestMatchers("/uploads/**").permitAll()
+                                    .requestMatchers("/api/v1/brands/**").permitAll()
+                                    .requestMatchers("/api/v1/categories/**").permitAll()
+                                    .requestMatchers("/api/v1/products/**").permitAll()
+                                    .anyRequest().authenticated()
+                    )
+                    .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                    .sessionManagement(sess ->
+                            sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            return http.build();
+        }
+
+
+
+        @Override
+        public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+            PageableHandlerMethodArgumentResolver pageableResolver = new PageableHandlerMethodArgumentResolver();
+            pageableResolver.setFallbackPageable(PageRequest.of(0, 10));
+            resolvers.add(pageableResolver);
+        }
+
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager (
-            AuthenticationConfiguration authConfig ) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/authenticate").permitAll()
-                                .requestMatchers("/users/**").permitAll()
-                                .requestMatchers("/v2/**").permitAll()
-                                .requestMatchers("/uploads/**").permitAll()
-                                .requestMatchers("/api/v1/brands/**").permitAll()
-                                .requestMatchers("/api/v1/categories/**").permitAll()
-                                .requestMatchers("/api/v1/products/**").permitAll()
-                )
-                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement(sess ->
-                        sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
-    }
-
-
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        PageableHandlerMethodArgumentResolver pageableResolver = new PageableHandlerMethodArgumentResolver();
-        pageableResolver.setFallbackPageable(PageRequest.of(0, 10));
-        resolvers.add(pageableResolver);
-    }
-
-}
