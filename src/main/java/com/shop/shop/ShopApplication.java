ded58ca694e95f5c73f6fe669cf47bb91793b217
tapash22 <tapasp263@gmail..com>
2026-04-26 15:10:03 +0800

    update fileconfig
    Signed-off-by: tapash22 <tapasp263@gmail..com>

--- src/main/java/com/shop/shop/ShopApplication.java
+++ src/main/java/com/shop/shop/ShopApplication.java
@@ -3,11 +3,27 @@ package com.shop.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class ShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	private static void createUploadDirectory() {
+		try {
+			Path uploadDir = Paths.get("upload/");
+			if (!Files.exists(uploadDir)) {
+				Files.createDirectories(uploadDir);
+				System.out.println("Upload directory created at: " + uploadDir.toAbsolutePath());
+			}
+		} catch (Exception e) {
+			System.err.println("Failed to create upload directory: " + e.getMessage());
+		}
 	}
 
 }
